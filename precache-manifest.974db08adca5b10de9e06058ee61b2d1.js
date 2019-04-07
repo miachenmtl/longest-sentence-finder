7360fe25112f1e1d1652f604ef6df94299443da6
Mia <mia.chen.mtl@gmail.com>
2019-04-07 18:33:41 -0400

    Updates

---
+++ precache-manifest.974db08adca5b10de9e06058ee61b2d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e01af37a6473c3d742b",
+    "url": "/longest-sentence-finder/static/js/runtime~main.16fccfc2.js"
+  },
+  {
+    "revision": "a770e43843621365ac45",
+    "url": "/longest-sentence-finder/static/js/main.4f4977fd.chunk.js"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/longest-sentence-finder/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "a770e43843621365ac45",
+    "url": "/longest-sentence-finder/static/css/main.88f53f10.chunk.css"
+  },
+  {
+    "revision": "8cbce675ab71d6ceab6b206e3958d845",
+    "url": "/longest-sentence-finder/index.html"
+  }
+];
